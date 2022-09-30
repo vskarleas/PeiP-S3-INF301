4847106f3f17e49a2cb707000ce891b79872255c
Vasilis Skarleas <51437323+vskarleas@users.noreply.github.com>
2022-09-30 20:39:03 +0200

    derniers commentaires

--- Client c/client-tutoriel.c
+++ Client c/client-tutoriel.c
@@ -8,7 +8,7 @@
 
 int main() {
     char reponse[MAXREP]; // pour stocker la réponse du serveur
-    //char message[MAXMSG]; // pour stocker le message à envoyer au serveur
+    char message[MAXMSG]; // pour stocker le message à envoyer au serveur
 
     // Affiche les échanges avec le serveur (false pour désactiver)
     mode_debug(true);
@@ -20,14 +20,32 @@ int main() {
 
     // Remplacez <identifiant> et <mot de passe> ci dessous.
     envoyer_recevoir("login 12108112 TREHIN", reponse);
-    char messages[100][100] = {"load tutoriel", "start", "oui", "4", "blanc", "Pincemoi", "tutoriel"};
-    int messages_len = 7;
+    char messages[100][100] = {"load TUTORIEL", "aide", "start", "OK", "OUI"};
+    int messages_len = 5;
     for (int i = 0; i < messages_len; i++)
     {
         envoyer_recevoir(messages[i], reponse);
     }
 
-    printf ("Reponse du serveur: %s", reponse);
+    int i;
+    for (int j = 0; j < 6; j++)
+    {
+        i = 0;
+        while (reponse[i] != '\0'){
+            char lettre = reponse[i];
+            if (isalpha(lettre)){
+                char lettre_majuscule = toupper(lettre);
+                message[i] = lettre_majuscule;
+            }else{
+                message[i] = lettre;
+            }
+            i++;
+        }
+        message[i] = '\0';
+        envoyer_recevoir(message, reponse);
+    }
+
+    printf ("Réponse du serveur: %s", reponse);
 
     printf ("Fin de la connection au serveur\n");
     return 0;
