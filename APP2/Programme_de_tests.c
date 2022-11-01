642faed8f4fa4be350f49a9ffcc305d08a213db5
Vasilis Skarleas <51437323+vskarleas@users.noreply.github.com>
2022-11-01 19:06:43 +0200

    Update Programme_de_tests.c

--- APP2/Programme_de_tests.c
+++ APP2/Programme_de_tests.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Programme de tester le comportement des nos fonctions avant l'execution de vraies tests.
+*/
 int main()
 {
     char texte[] = "{ 1 M { D } { A } ? }{XC 32R !C !} C !";
     sequence_t *seq = malloc(sizeof(sequence_t));
-    conversion(texte, seq);
+    conversion(texte, seq); //Changement de l'appele par rapport le test qu'on voudrait faire chaque fois
     afficher(seq);
     printf("\n");
 }
